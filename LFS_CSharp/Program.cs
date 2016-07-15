8c135ba325ac3ccc6d57578630c51907cfefba4d
Blobby19 <luc.viala19@gmail.com>
2016-07-15 23:56:45 +0200

    Adding UDP Sockets but not working anymore

--- LFS_CSharp/Program.cs
+++ LFS_CSharp/Program.cs
@@ -2,21 +2,44 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.IO;
+using System.Configuration;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Net.NetworkInformation;
 
 namespace LFS_CSharp
 {
     static class Program
     {
+
+        static UdpClient client;
+        static IPEndPoint endpoint;
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            endpoint = new IPEndPoint(IPAddress.Any, 29967);
+            client = new UdpClient(endpoint);
+            client.BeginReceive(new AsyncCallback(DataReceived), endpoint);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
+
+        private static void DataReceived(IAsyncResult ar)
+        {
+            byte[] bResp = client.EndReceive(ar, ref endpoint);
+            string sResponse = Encoding.UTF8.GetString(bResp);
+            Console.WriteLine(sResponse);
+            client.Close();
+        }
     }
+
+    
 }
